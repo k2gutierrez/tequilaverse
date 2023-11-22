13a55ac2479a2050599fd70adf85470078b4066c
k2gutierrez <cchimal.gutierrez@gmail.com>
2023-11-22 04:54:14 -0600

    changed a meta in head in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,6 +33,9 @@ const wagmiConfig = createConfig({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
       <body >
         <>
           <WagmiConfig config={wagmiConfig} >
